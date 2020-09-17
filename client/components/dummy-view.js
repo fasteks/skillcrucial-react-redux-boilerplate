25dd6d5a4659842ee496b4efc95efc9581b5dc99
fasteks <fastik-07@mail.ru>
2020-09-18 00:03:59 +0300

    runned yarn remove history/yarn upgrade to resolve react router link working problem

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -10,9 +10,7 @@ const Dummy = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 hover:text-red-500 text-white font-bold rounded-lg border shadow-lg p-10">
           This is dummy component
-        </div>
-        <div>
-          <Link to="/">Go to Root</Link>
+          <Link to="/dashboard">Go to dashboard</Link>
         </div>
       </div>
     </div>
